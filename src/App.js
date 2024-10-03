c1e153284d2afa6ea52ab5ba552113972b631303
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-03 15:37:02 +0500

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import './App.css'
 
 function App () {
-  let name = 'Hamza Liaqat'
   return (
     <>
-      <nav className='navBar'>
-        <li>Home</li>
-        <li>Course</li>
-        <li>My Portal</li>
-      </nav>
-
-      <div className='container'>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quidem
-          qui quo aspernatur cumque repellat provident totam, ad impedit
-          mollitia iure ea laudantium at vel sed atque neque rem. Quisquam?
-        </p>
-        author: {name}
-      </div>
     </>
   )
 }
