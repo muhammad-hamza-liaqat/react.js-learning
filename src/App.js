965262661d5137b8d18f259c2652622e12f10d0a
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-07 12:38:14 +0500

    dark Mode with 2 use state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css'
+import About from './components/About'
 import NavBar from './components/NavBar'
-import TextForm from './components/TextForm'
+// import TextForm from './components/TextForm'
 
 function App() {
   return (
     <>
       <NavBar name='express.js docs' about='about express.js' />
       <div className="container">
-        <TextForm heading='Enter Text to Analyze' />
-
+        {/* <TextForm heading='Enter Text to Analyze' /> */}
+        <About/>
       </div>
     </>
   )
