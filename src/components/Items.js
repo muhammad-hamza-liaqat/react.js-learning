2c44891bbfa64a5794106da814909e46dea481e7
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-09 11:54:37 +0500

    functionality -> added to cart completed

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,24 +1,99 @@
-import React from 'react'
-import "./css.css";
+import React, { useContext } from 'react'
+import './css.css'
+import { CartContext } from '../context/CartContext'
 
-export default function Items() {
-    return (
-        <>
-            <div className="container my-4">
-                <div className="list-group">
-                    <a href="http://localhost:3000/" className="list-group-item list-group-item-action">Items</a>
+export default function Items () {
+  const cart = useContext(CartContext)
+  console.log('----->', cart)
 
-                    <a href="http://localhost:3000/" className="list-group-item list-group-item-action list-group-item-primary">Samsung Galaxy S24 ULTRA</a>
-                    <a href="http://localhost:3000/" className="list-group-item list-group-item-action list-group-item-secondary">Huawei P10</a>
-                    <a href="http://localhost:3000/" className="list-group-item list-group-item-action list-group-item-success">Nokia 3310</a>
-                    <a href="http://localhost:3000/" className="list-group-item list-group-item-action list-group-item-danger">BlackBerry E15</a>
-                    <a href="http://localhost:3000/" className="list-group-item list-group-item-action list-group-item-warning">Oppo F15</a>
-                    <a href="http://localhost:3000/" className="list-group-item list-group-item-action list-group-item-info">Sony E12</a>
-                    <a href="http://localhost:3000/" className="list-group-item list-group-item-action list-group-item-light">Vivo y12</a>
-                    <a href="http://localhost:3000/" className="list-group-item list-group-item-action list-group-item-dark">IPhone XS</a>
-                </div>
-            </div>
+  const addToCartHandler = item => {
+    cart.setItems([...cart.items, item])
+  }
+  return (
+    <>
+      <div className='container my-4'>
+        <div className='list-group'>
+          <a
+            href='#'
+            className='list-group-item list-group-item-action d-flex justify-content-between align-items-center'
+          >
+            Items
+          </a>
 
-        </>
-    )
+          <a
+            href='#'
+            className='list-group-item list-group-item-action list-group-item-primary d-flex justify-content-between align-items-center'
+          >
+            Samsung Galaxy S24 ULTRA
+            <button className='btn btn-primary' onClick={addToCartHandler}>
+              Add to Cart
+            </button>
+          </a>
+          <a
+            href='#'
+            className='list-group-item list-group-item-action list-group-item-secondary d-flex justify-content-between align-items-center'
+          >
+            Huawei P10
+            <button className='btn btn-secondary' onClick={addToCartHandler}>
+              Add to Cart
+            </button>
+          </a>
+          <a
+            href='#'
+            className='list-group-item list-group-item-action list-group-item-success d-flex justify-content-between align-items-center'
+          >
+            Nokia 3310
+            <button className='btn btn-success' onClick={addToCartHandler}>
+              Add to Cart
+            </button>
+          </a>
+          <a
+            href='#'
+            className='list-group-item list-group-item-action list-group-item-danger d-flex justify-content-between align-items-center'
+          >
+            BlackBerry E15
+            <button className='btn btn-danger' onClick={addToCartHandler}>
+              Add to Cart
+            </button>
+          </a>
+          <a
+            href='#'
+            className='list-group-item list-group-item-action list-group-item-warning d-flex justify-content-between align-items-center'
+          >
+            Oppo F15
+            <button className='btn btn-warning' onClick={addToCartHandler}>
+              Add to Cart
+            </button>
+          </a>
+          <a
+            href='#'
+            className='list-group-item list-group-item-action list-group-item-info d-flex justify-content-between align-items-center'
+          >
+            Sony E12
+            <button className='btn btn-info' onClick={addToCartHandler}>
+              Add to Cart
+            </button>
+          </a>
+          <a
+            href='#'
+            className='list-group-item list-group-item-action list-group-item-light d-flex justify-content-between align-items-center'
+          >
+            Vivo y12
+            <button className='btn btn-light' onClick={addToCartHandler}>
+              Add to Cart
+            </button>
+          </a>
+          <a
+            href='#'
+            className='list-group-item list-group-item-action list-group-item-dark d-flex justify-content-between align-items-center'
+          >
+            IPhone XS
+            <button className='btn btn-dark' onClick={addToCartHandler}>
+              Add to Cart
+            </button>
+          </a>
+        </div>
+      </div>
+    </>
+  )
 }
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -2,9 +2,10 @@ import { createContext, useState } from "react";
 
 export const CartContext = createContext();
 
-export const CartProvider = (props) =>{
+export const CartProvider = (props) => {
+    const [items, setItems] = useState([]);
     return (
-        <CartContext.Provider >
+        <CartContext.Provider value={{ items, setItems }}>
             {
                 props.children
             }
