213d3cc94a831312bdce5dd0a93454aea81b3a6d
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-09 14:35:06 +0500

    useRef implementation

--- src/components/CounterCard.js
+++ src/components/CounterCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./css.css";
 export default function CounterCard() {
     const [count, setCount] = useState(0);
@@ -12,11 +12,21 @@ export default function CounterCard() {
         console.log("substractOneHandler called")
         setCount(currentState => (currentState > 0 ? currentState - 1 : 0))
     }
-    
-    const resetHandler = () =>{
+
+    const resetHandler = () => {
         console.log("reset handler called");
         setCount(0);
     }
+
+    // using the useRef allow to presist the state, 
+    // when the count value is increased everytime, the component is re-rendered, the other local variables 
+    // loses data when there is re render. useRef allows us to persist the state. such as below.....
+    let a = useRef(0)
+    useEffect(() => {
+        console.log(`Component redendered again`)
+    }, [])
+    a.current = a.current + 1
+    console.log(`value of ${a.current}`)
 
     return (
         <>
