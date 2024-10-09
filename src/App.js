7c83e3cecb088003c4a6b152516f39424cfe4a55
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-09 10:54:46 +0500

    global state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { useContext } from "react"
 import Counter from "./components/Counter"
 import NavBar from "./components/NavBar"
 import "./components/css.css"
+import { counterContext } from "./context/context"
 function App() {
+  const counterState = useContext(counterContext)
+  console.log(counterState, "counter value from createContext")
   return (
     <>
       <NavBar />
       <div className="container">
+        <h1 className="app-head1">{counterState.count}</h1>
         <Counter />
         <Counter />
         <Counter />
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import "./css.css";
+import React, { useContext } from 'react'
+import './css.css'
+import { counterContext } from '../context/context'
 
 export default function Counter() {
+    const counterState = useContext(counterContext)
+    const addOneHandler = () => {
+        counterState.setCount(() => counterState.count + 1)
+    }
+    const subOneHandler = () => {
+        if (counterState.count === 0) return 
+        counterState.setCount(() => counterState.count - 1)
+
+    }
     return (
         <>
-            <div className="btn">
-                <button className='addBtn'>+ 1</button>
-                <button className='subBtn'>- 1</button>
+            <div className='btn'>
+                <button className='addBtn' onClick={addOneHandler}>
+                    + 1
+                </button>
+                <button className='subBtn' onClick={subOneHandler}>
+                    - 1
+                </button>
             </div>
         </>
     )
