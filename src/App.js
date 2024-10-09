10fcd3e786c2860ad93380dafadb42599614f2bd
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-09 11:04:42 +0500

    setup card-context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import CounterCard from "./components/CounterCard"
 import NavBar from "./components/NavBar"
 import "./components/css.css"
 function App() {
   return (
     <>
       <NavBar />
-      <div className="container">
-        <CounterCard />
-      </div>
     </>
   )
 }
