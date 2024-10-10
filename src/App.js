1f22c556f682cb9b8e3bcc538ef356b7be8b9c28
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-10 10:18:12 +0500

    basic code setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import './App.css'
 import NavBar from './components/NavBar'
-import TextForm from './components/TextForm'
 
 function App() {
   return (
     <>
       <NavBar name='express.js docs' about='about express.js' />
-      <div className="container">
-        <TextForm heading='Enter Text to Analyze' />
-
-      </div>
     </>
   )
 }
