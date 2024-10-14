4c4a20f4a73dc73bb44a19be94b951c9ae5d6c45
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-14 14:17:49 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import CardCounter from './components/CardCounter'
 import NavBar from './components/NavBar'
-import ReducerCounter from './components/ReducerCounter'
 import "./style/components.css"
 
 function App() {
   return (
     <>
       <NavBar name='express.js docs' about='about express.js' />
-      <div className="cardCounter">
-        {/* <CardCounter /> */}
-        <ReducerCounter/>
-      </div>
     </>
   )
 }
